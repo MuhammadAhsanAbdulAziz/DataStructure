48be3852705afc6f4639fba5bf12a0b87118a483
muhammadahsanabdulaziz <maahsan36@gmail.com>
2022-10-22 18:27:09 +0500

    made some changes

--- Graphs/makingGraphFromFile.cpp
+++ Graphs/makingGraphFromFile.cpp
@@ -3,15 +3,18 @@
 #include <sstream>
 using namespace std;
 
+// The coordinates file should follow a proper syntax
+
+
 int main()
 {
     ifstream inputFile("Coordinates.txt");
 
     string line;
-    int rows, cols,x,y,val;
+    int rows, cols, x, y, val;
     int **arr;
 
-    getline(inputFile,line);
+    getline(inputFile, line);
     if (getline(inputFile, line))
     {
         istringstream ss(line);
@@ -26,30 +29,61 @@ int main()
     inputFile.seekg(0);
     getline(inputFile, line);
 
-    
-    if(line == "w" || line == "W")
+    // if Graph is Weighted and Directed
+
+    if (line == "w dg" || line == "W DG")
     {
-        getline(inputFile, line);
-        getline(inputFile, line);
+
+        getline(inputFile, line); // skipping the second line containing the dimensions of array
         while (getline(inputFile, line))
-        {        
+        {
             istringstream ss(line);
             ss >> x >> y >> val;
             arr[x][y] = val;
         }
     }
-    else
+    // if Graph is Weighed and Undirected
+
+    else if (line == "w ug" || line == "W UG")
     {
-        getline(inputFile, line);
-        getline(inputFile, line);
+        getline(inputFile, line); // skipping the second line containing the dimensions of array
         while (getline(inputFile, line))
-        {        
+        {
+            istringstream ss(line);
+            ss >> x >> y >> val;
+            arr[x][y] = val;
+            arr[y][x] = val;
+        }
+    }
+
+    // if Graph is Unweighted and Directed
+
+    else if (line == "uw dg" || line == "UW DG")
+    {
+
+        getline(inputFile, line); // skipping the second line containing the dimensions of array
+        while (getline(inputFile, line))
+        {
             istringstream ss(line);
             ss >> x >> y ;
             arr[x][y] = 1;
         }
     }
-    
 
-    cout << arr[0][4];
+    // if Graph is Unweighted and Undirected
+
+    else if (line == "uw ug" || line == "UW UG")
+    {
+        getline(inputFile, line); // skipping the second line containing the dimensions of array
+        while (getline(inputFile, line))
+        {
+            istringstream ss(line);
+            ss >> x >> y;
+            arr[x][y] = 1;
+            arr[y][x] = 1;
+        }
+    }
+
+    cout << arr[0][4] << "\n";
+    cout << arr[4][0];
 }
